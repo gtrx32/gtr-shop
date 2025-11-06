3f5dabddd36e65d00b86f054be90ab7a286ef4d2
gtr <gtrx32@bk.ru>
2025-11-06 11:14:39 +0300

    модель товара + mfs

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+      'name',
+      'slug',
+      'description',
+      'price',
+      'stock',
+      'image'
+    ];
+
+    protected static function booted(): void
+    {
+        static::creating(function ($product) {
+            if (empty($product->slug)) {
+                $product->slug = Str::slug($product->name);
+            }
+        });
+    }
+}
---
+++ backend/database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = fake()->words(3, true);
+
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => fake()->text(),
+            'price' => fake()->randomFloat(2, 1, 100000),
+            'stock' => fake()->numberBetween(0, 100)
+        ];
+    }
+}
