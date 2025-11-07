7452eca5d253e8f7b42cd0cfc0d12e1f0a8d317d
gtr <gtrx32@bk.ru>
2025-11-07 10:39:35 +0300

    модель корзины + mfs

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(ReviewSeeder::class);
+        $this->call(CartSeeder::class);
     }
 }
