5d61f56f072a5c6d763aafe2c0fe634a16e2b5fe
gtr <gtrx32@bk.ru>
2025-11-19 15:24:52 +0300

    сменил faker на fake() в userFactory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -30,8 +30,8 @@ class UserFactory extends Factory
         Storage::disk('public')->put($filename, $image);
 
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
