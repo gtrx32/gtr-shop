326e8dc6bf3844854e342a7e4cca2402528adf14
gtr <gtrx32@bk.ru>
2025-12-04 16:49:21 +0300

    подключил тайлвинд, шрифты, создал пустой шаблон

---
+++ frontend/app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen grid grid-rows-[auto_1fr_auto] bg-gray-50">
+    <AppHeader />
+
+    <main class="px-4 py-6 max-w-6xl mx-auto">
+      <slot />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from '../components/layout/AppHeader.vue';
+import AppFooter from '../components/layout/AppFooter.vue';
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'GtrShop',
+    },
+  },
+
+  css: ['~/assets/css/main.css', '~/assets/css/fonts.css', '~/assets/css/reset.css'],
+  modules: ['@nuxtjs/tailwindcss'],
+});
