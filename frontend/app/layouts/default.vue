deae2ed496a265d9e1795bd2d2d2a64f0f51c1b7
gtr <gtrx32@bk.ru>
2026-01-13 18:00:50 +0300

    пофиксил отступы в леяуте

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -4,10 +4,10 @@ import AppFooter from '../components/layout/AppFooter.vue';
 </script>
 
 <template>
-  <div class="min-h-[100dvh] flex flex-col gap-6 bg-gray-50">
+  <div class="min-h-[100dvh] flex flex-col gap-8 sm:gap-12 bg-gray-50">
     <AppHeader />
 
-    <main class="flex-1 max-w-7xl mx-auto w-full px-4 sm:px-6">
+    <main class="flex-1 max-w-7xl mx-auto w-full px-4 sm:px-8">
       <slot />
     </main>
 
