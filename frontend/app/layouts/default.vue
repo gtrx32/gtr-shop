a3e5ae04ab79cb902a9b917ff29192dde2028ce2
gtr <gtrx32@bk.ru>
2026-01-30 14:40:04 +0300

    переименовал хедер и футер

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import AppHeader from '../components/layout/AppHeader.vue';
-import AppFooter from '../components/layout/AppFooter.vue';
+import Header from "~/components/layout/default/Header.vue";
+import Footer from "~/components/layout/default/Footer.vue";
 </script>
 
 <template>
   <div class="min-h-[100dvh] flex flex-col gap-8 sm:gap-12 bg-gray-50">
-    <AppHeader />
+    <Header />
 
     <main class="flex-1 max-w-7xl mx-auto w-full px-4 sm:px-8">
       <slot />
     </main>
 
-    <AppFooter />
+    <Footer />
   </div>
 </template>
